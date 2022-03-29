d415b1333c9b32fd1772c1e66813db73e7c0974e
JobsonEmanoel <94983068+JobsonEmanoel@users.noreply.github.com>
2022-03-29 09:43:42 -0300

    cadastro

---
+++ script.js
@@ -0,0 +1,21 @@
+const idade = 18
+if(idade >= 18){
+    console.log("Voce tem permição")
+}
+else{
+    console.log("Sem permição")
+}
+const data = "28/03/2022"
+if(data <= "28/03/2022" ){
+    console.log("Dentro do prazo")
+}
+else{
+    console.log("Prazo expirado")
+}
+const participante = 100
+if(participante <= 100){
+    console.log("Pode se cadastrar")
+}
+else{
+    console.log("não pode se cadastrar")
+}
---
+++ test.js
@@ -0,0 +1,9 @@
+let dataAtual = 28/03/2022
+let dataLimite = 30/03/2022
+
+if(dataAtual < dataLimite){
+    console.log("Evento esta finalizado")
+    dataAtual = false
+}else{
+    dataAtual = true;
+}
